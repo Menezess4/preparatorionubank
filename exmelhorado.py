ce784b296dd5ffaa64186047ac1d2f45f1472862
menezess <gutamenezes16@gmail.com>
2026-09-01 17:37:30 -0300

    Update exmelhorado.py

--- exmelhorado.py
+++ exmelhorado.py
@@ -1,10 +1,12 @@
 
 contas = {}
+historico_contas={}
 def criar_contas(nome):
     if nome in contas:
         print('Conta ja existe')
         return (False)
     contas[nome] = 0
+    historico_contas[nome]=[]
 criar_contas('Alice')
 criar_contas('bob')
 print(contas)
@@ -15,6 +17,7 @@ def depositar(nome,valor):
             return (False)
         else:
             contas[nome] += valor
+            historico_contas[nome].append(("Deposito",valor))
             print(f'O valor de {valor} atribuido com sucesso')
     else:
         print('Conta n existe na tabela')
@@ -33,9 +36,11 @@ def sacar(nome,valor):
         print('Conta n existe na tabela')
         return (False)
     if valor > contas[nome] :
+            
             print('Valor inexistente')
             return(False)
     contas[nome] -= valor
+    historico_contas[nome].append(("sacou", valor))
 sacar('Alice',500)
 print(contas)
 def consultar_saldo(nome):
@@ -58,7 +63,53 @@ def transferir(ori, dest, valor):
         return (False)  
     contas[ori]-=valor
     contas[dest]+=valor
+    
+    historico_contas[ori].append((valor,"foi tranferido para",dest))
+    historico_contas[dest].append((valor,"foi recebido de",ori))
+
+
     return(True)
 
 transferir("Alice", "bob", 200)
 print(contas)
+def calcular_total():
+    tudo = 0
+
+    for saldo in contas.values():
+        tudo += saldo
+
+    print("Saldo disponivel:", tudo)
+    return(tudo)
+calcular_total()
+def maior_saldo():
+    maior=0
+    contas_maior=None
+    for nome,saldo in contas.items():
+        if saldo>maior:
+            maior=saldo
+            contas_maior=nome
+
+    print(f"O {contas_maior} tem o maior saldo de: {maior}")
+    return(maior)
+maior_saldo()
+
+def historico(nome):
+    if nome not in contas :
+        return(False)
+    return historico_contas[nome]
+print(historico("Alice"))
+print(historico("bob"))
+
+def resumo_conta(nome):
+    if nome not in contas :
+        return(False)
+    quant=len(historico_contas[nome]) 
+    resumo={
+    "nome":nome,
+    "Saldo":contas[nome],
+    "Quantidade de operacoes":quant
+    }  
+    print(resumo_conta("Alice"))
+     
+   
+    return resumo
